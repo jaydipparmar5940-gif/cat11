e1ebfe24ba5ac134024ab7de1a5f052e63008e8b
Jaydip Parmar <jaydipparmar5940@gmail.com>
2026-03-23 18:54:33 +0530

    Refactor: Demolish legacy CricAPI service module and supplementary cron jobs in favor of RapidAPI routing

--- backend/test-cric.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const srv = require('./src/services/cricapi.service');
-
-srv.getUpcomingMatches()
-    .then(matches => console.log(`[TEST] Fetched ${matches.length} matches`))
-    .catch(err => console.error('[TEST] Fatal:', err))
-    .finally(() => process.exit(0));
